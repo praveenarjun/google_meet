987cb1ae786e14bef66d945de5112629829602b4
praveenchalla <sreepraveenchalla@gmail.com>
2025-01-03 21:10:15 +0530

    Google Meet Using React Native

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    project:{
+        ios:{},
+        android: {},
+    },
+    assets: ['./src/assets/fonts/'],
+    getTransformModulePath(){
+        return require.resolve('react-native-typescript-transformer');
+
+    },
+    getSourceExts(){
+        return ['ts','tsx'];
+    },
+};
